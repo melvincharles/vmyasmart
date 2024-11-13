746d364e2c13a5ca0093d34cf6e3978df2be4c0e
melvin <melvin.mca88@gmail.com>
2024-11-14 00:36:13 +0100

    Rename project from joes_project to syam_project

--- syam_project/settings.py
+++ syam_project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'joes_project.urls'
+ROOT_URLCONF = 'syam_project.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'joes_project.wsgi.application'
+WSGI_APPLICATION = 'syam_project.wsgi.application'
 
 
 # Database
